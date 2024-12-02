5d39b63072bdc716a785e84c5c16856d51644efb
Lobana Alarabi <loubanaeng@gmail.com>
2024-12-02 17:21:54 +0300

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -18,16 +18,16 @@ return (
                   <div className="container" style={{margin:"1em"}}>
                       <div className="row" style={{margin:"1em"}}>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Profile</h3>
-                            <span className="colorChange">Female | 9/march/1990 | syrian</span>
+                            <span className="colorChange">Female | syrian</span>
                             <p className="colorChange fontMobile">My name is Lobana Alarabi.A Web Designer Junior.I was born in Aleppo Syria,
                           and now living in Turkey.I studied computer engineering in Aleppo Unevirsity my specialize
                           in artificial intelligence. Programing is my life,keeping learning more and more..</p>
                           </div>
                         </div>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Technical Skills</h3>
                             <span className="colorChange"></span>
                             <p className="colorChange fontMobile">Good command of Microsoft Visual studio, Sql server 2008, Matlab.
@@ -36,7 +36,7 @@ return (
                           </div>
                         </div>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">WorkShop</h3>
                             <span className="colorChange"></span>
                             <p className="colorChange fontMobile"><ul >
@@ -52,7 +52,7 @@ return (
                   
                       <div className="row" style={{margin:"1em"}}>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Work Experience</h3>
                             <span className="colorChange">Assistant Charge Affaires–Full Time</span>
                             <p className="colorChange fontMobile">AT Mathematical Statistics Department <a href="https://alepuniv.edu.sy/view-faculty/college-of-science">College of Science</a> I taught several subjects for
@@ -65,7 +65,7 @@ return (
                           </div>
                         </div>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Work Experience</h3>
                             <span className="colorChange">Trainer at ITC Teaching institute</span>
                             <p className="colorChange fontMobile">Worked as a trainer in <a href="http://www.itc-center.com/">ITC</a> for two courses 
@@ -74,7 +74,7 @@ return (
                           </div>
                         </div>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Work Experience</h3>
                             <span className="colorChange">Sales Officer at Alghareb Travel Agency</span>
                             <p className="colorChange fontMobile"> I work as a social media marketing – designer at <a href="http://www.alghareb-tourism.com/">Alghareb Agency</a> 
@@ -87,7 +87,7 @@ return (
 
                       <div className="row" style={{margin:"1em"}}>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Education</h3>
                             <span className="colorChange"> B.A Degree (2008 - 2013)</span>
                             <p className="colorChange fontMobile"> B.A. Informatics engineering at the <a href="https://alepuniv.edu.sy/view-faculty/faculty-of-informatics-engineering">Aleppo University</a> with
@@ -96,7 +96,7 @@ return (
                           </div>
                         </div>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Education</h3>
                             <span className="colorChange">(Flatiron School - NY, USA) 2019 - 2020</span>
                             <p className="colorChange fontMobile">Re:Coded Bootcamp Istanbul, Turkey 
@@ -107,7 +107,7 @@ return (
                           </div>
                         </div>
                         <div className="col-md-4 mobileres">
-                          <div className="cardR cardR-1">
+                          <div className="cardR ">
                             <h3 className="colorChange">Languages</h3>
                             <span className="colorChange"></span>
                             <p className="colorChange fontMobile">Arabic native - English Advanced - Turkish intermediate</p>
